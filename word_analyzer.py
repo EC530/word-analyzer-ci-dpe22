bed3b1417f926ce3d701d9a7273d3b8378226b06
dpe22 <dpe22@bu.edu>
2022-01-29 17:20:07 -0500

    updated word_analyzer function

--- word_analyzer.py
+++ word_analyzer.py
@@ -1,2 +1,25 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk import ngrams
+from collections import Counter
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
 def word_analyzer(document):
-    pass
+
+    document = "".join(document)
+    new_tokens = word_tokenize(document)
+    new_tokens = [t.lower() for t in new_tokens]
+    new_tokens = [t for t in new_tokens if t not in stopwords.words('english')]
+    new_tokens = [t for t in new_tokens if t.isalpha()]
+    lemmatizer = WordNetLemmatizer()
+    new_tokens = [lemmatizer.lemmatize(t) for t in new_tokens]
+    counted = Counter(new_tokens)
+    word_freq = pd.DataFrame(counted.items(),columns=['word','frequency']).sort_values(by='frequency',ascending=False)
+
+    fig, axes = plt.subplots(3,1,figsize=(8,20))
+    sns.barplot(ax=axes[0],x='frequency',y='word',data=word_freq.head(30))
+
+    return word_freq
