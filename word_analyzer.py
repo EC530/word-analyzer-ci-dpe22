33c608e836c106ced5b43ff9a7623c4ed1073164
dpe22 <dpe22@bu.edu>
2022-01-29 20:05:23 -0500

    add exception

--- word_analyzer.py
+++ word_analyzer.py
@@ -11,12 +11,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
 def word_analyzer(document):
+
+    filesize = os.path.getsize(document)
+    if filesize == 0: 
+        raise Exception("The file is empty, please try again with another text file.")
 
     # open the document and tokenize words using nltk
     with open(document) as f:
